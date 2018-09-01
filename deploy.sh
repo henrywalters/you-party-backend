1b6457df11476775ac02219d051f70873277ac64
henrywalters <henrywalters20@gmail.com>
2018-09-01 14:01:27 -0400

    Debugging why sublist not showing

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,19 @@ if [ "$1" != "" ]; then
     tsc
     echo "Deploying to github & heroku with commit message: "
     echo $1
-    echo "Staging for commit"
-    git add .
-    echo "Commiting"
-    git commit -m "$1"
-    echo "Pushing to github"
-    git push
-    echo "Pushing to Heroku"
-    git push heroku master
-    echo "Success"
+    if [ -n "$(git status --porcelain)" ]; then
+        echo "Staging for commit"
+        git add .
+        echo "Commiting"
+        git commit -m "$1"
+        echo "Pushing to github"
+        git push
+        echo "Pushing to Heroku"
+        git push heroku master
+        echo "Success"
+    else
+        echo "No changes detected. Ending Process";
+    fi
 
 else
     echo "Please leave a commit message"
